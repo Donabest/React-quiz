bbd8a19826f9a1aa3f7e563ccc2673c0ccbd10f3
Donabest <rabiuazeez3@gmail.com>
2025-12-14 15:45:04 +0100

    feat:features

--- my-app/src/components/Progress.jsx
+++ my-app/src/components/Progress.jsx
@@ -1,6 +1,10 @@
-function Progress({ index, numQuestions, points, maxPossilePoints }) {
+function Progress({ index, numQuestions, points, maxPossilePoints, answer }) {
   return (
     <header className="progress">
+      <progress
+        max={numQuestions}
+        value={index + Number(answer !== null)}
+      ></progress>
       <p>
         Question <strong>{index + 1}</strong>/ {numQuestions}
       </p>
